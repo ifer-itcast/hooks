152ed088e89191fdeb3201bfae7339d53332e8fc
ifer-itcast <yangkangkang@itcast.cn>
2020-04-02 18:28:39 +0800

    useContext

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component, useState, createContext, useContext } from 'react';
+const CountContext = createContext();
 
-function Test() {
-	const [count, setCount] = useState(0);
-	const onClick = () => {
-		console.log('一旦绑定事件的元素被切换了，这里就失效了');
-	};
-    useEffect(() => {
-        document.querySelector('#size').addEventListener('click', onClick);
-        return () => {
-            document.querySelector('#size').removeEventListener('click', onClick);
-        };
-    }, [count]);
-	return (
-		<div>
-			<p>
+// 基础写法
+class Foo extends Component {
+	render() {
+		return (
+			<CountContext.Consumer>
+				{count =>
+					<h1>
+						{count}
+					</h1>}
+			</CountContext.Consumer>
+		);
+	}
+}
+// 稍优雅写法
+class Bar extends Component {
+	static contextType = CountContext;
+	render() {
+		const count = this.context;
+		return (
+			<h1>
 				{count}
-			</p>
-			<button
-				onClick={() => {
-					setCount(count + 1);
-				}}
-			>
-				click
-			</button>
-			{count % 2 ? <div id="size">hello world2</div> : <p id="size">hello world1</p>}
-		</div>
+			</h1>
+		);
+	}
+}
+
+// Hooks 写法
+function Counter() {
+	const count = useContext(CountContext);
+	return (
+		<h1>
+			{count}
+		</h1>
 	);
 }
 
 function App() {
-	return <Test />;
+	const [count, setCount] = useState(0);
+	return (
+		<div>
+			<button onClick={() => setCount(count + 1)}>
+				click {count}
+			</button>
+			<CountContext.Provider value={count}>
+				<Foo />
+				<Bar />
+				<Counter />
+			</CountContext.Provider>
+		</div>
+	);
 }
-
 export default App;
